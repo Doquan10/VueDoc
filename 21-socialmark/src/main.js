8662044a6bca5d92c0b01a0b377699d194e675af
dogukan <doquangoncu@gmail.com>
2023-07-04 18:01:58 +0300

    04.07

--- 21-socialmark/src/main.js
+++ 21-socialmark/src/main.js
@@ -1,7 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+import "@/assets/style.css";
+import appHeader from "@/components/Shared/appHeader"
+import appBookmarkList from "@/components/Shared/appBookmarkList"
+
+
+
+const app = createApp(App);
+app.component("appHeader",appHeader);
+app.component("appBookmarkList",appBookmarkList);
+app.use(router);
+
+app.mount("#app");
--- 21-socialmark/src/router/index.js
+++ 21-socialmark/src/router/index.js
@@ -1,26 +1,34 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    {
-        name:"HomePage",
-        path:"/",
-        component: () =>import("@/views/Home")
-    },
-    {
-        name:"LoginPage",
-        path:"/login",
-        component: () =>import("@/views/Login")
-    },
-    {
-        name:"RegisterPage",
-        path:"/register",
-        component: () =>import("@/views/Register")
-    }
+  {
+    name: "HomePage",
+    path: "/",
+    component: () => import("@/views/Home")
+  },
+  {
+    name: "LoginPage",
+    path: "/login",
+    component: () => import("@/views/Login")
+  },
+  {
+    name: "RegisterPage",
+    path: "/register",
+    component: () => import("@/views/Register")
+  },
+  {
+    name: "NewBookmarkPage",
+    path: "/new",
+    component: () => import("@/views/NewBookmark")
+  }
+  
 ];
 
-
-export default createRouter({
-    routes,
-
-    history: createWebHistory()
+const router = createRouter({
+  routes,
+  history: createWebHashHistory()
 });
+
+
+
+export default router;
