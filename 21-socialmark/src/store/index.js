e6bf70704b248bd7b7fca48703290bcc4bf60eae
Doğukan Göncü <doquangoncu@gmail.com>
2023-07-06 12:15:06 +0300

    06-07-1

--- 21-socialmark/src/store/index.js
+++ 21-socialmark/src/store/index.js
@@ -1,17 +1,26 @@
 import {createStore} from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
     state : {
         user: null,
         saltKey :"booklike123**?09"
     },
+    mutations :{
+        setUser(state,user){
+            console.log("user :>> ", user);
+            state.user = user;
+        }
+    },
     getters :{
-        _isAuthenticated : state => state.user != null,
+        _isAuthenticated: state => state.user !== null,
         _getCurrentUser(state){
             const user = state.user;
             delete user?.password;
             return user;
         },
         _saltKey : state => state.saltKey,
-    }
+    },
+    plugins:[createPersistedState()]
+
 });
