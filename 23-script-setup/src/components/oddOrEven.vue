5fb570bdc325299caef65d0cff77dd0a95643d88
dogukan <doquangoncu@gmail.com>
2023-07-23 20:45:02 +0300

    23.07-1

---
+++ 23-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>asd</h1>
+  <h3>{{counter}}</h3>
+  <h3>{{result}}</h3>
+</template>
+
+<script setup>
+import {computed, watch} from "vue";
+import { defineProps } from "vue";
+import { defineEmits } from "vue";
+const props = defineProps({ counter: Number });
+const emit = defineEmits(["odd-event"]);
+
+const result = computed(() => (props.counter % 2 == 0 ? "Çift" : "Tek"));
+
+watch(result, (result) => {
+  if(result == "Tek") emit("odd-event", true);
+});
+</script>
