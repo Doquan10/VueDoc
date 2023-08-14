17c1383d0360176447cd069c2a2120a2f5a1d84c
doquan <doquangoncu@gmail.com>
2023-08-14 18:03:15 +0300

    14.08

--- file-upload/src/components/HelloWorld.vue
+++ file-upload/src/components/HelloWorld.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <input type="file" @change="handleFileUpload" />
+    <input type="file" @change="handleFileUpload"  />
     <button @click="uploadFile">Dosyayı Yükle</button>
+    <button @click="convertZip">Zip Çevir</button>
+  
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import JSZip from 'jszip';
+
 export default {
-  
+
   data() {
     return {
       selectedFile: null,
@@ -20,19 +24,62 @@ export default {
     },
     uploadFile() {
       // Dosya yükleme işlemini gerçekleştirme
-    const formData = new FormData();
-    formData.append('file', this.selectedFile);
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
 
-    axios.post('https://localhost:7002/FileUpload', formData)
-      .then(response => {
-        // Dosya yükleme başarılı
-        alert(response);
-      })
-      .catch(error => {
-        // Dosya yükleme sırasında bir hata oluştu
-        alert(error);
-      });
+          axios.post('https://localhost:7239/api/Upload/UploadFile', formData, {
+            headers: {
+
+              'Content-Type': 'multipart/form-data'
+              // If you receieve JSON response.
+            }
+          })
+            .then(response => {
+              // Dosya yükleme başarılı
+              alert(response);
+            })
+            .catch(error => {
+              // Dosya yükleme sırasında bir hata oluştu
+              alert(error);
+            });
+     
+
     },
+    convertZip() {
+      /*
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+      var zip = new JSZip();
+      zip.file( formData);
+      zip.generateAsync({ type: 'blob' }).then(content => {
+        // Oluşturulan zip dosyasını indirme işlemi
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(content);
+        link.download = 'myZipFile.zip';
+        link.click();
+        URL.revokeObjectURL(link.href);
+      });
+      */
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+      const file = new Blob([formData], { type: 'application/pdf' });
+      const zip = new JSZip();
+      zip.file('userFile.pdf', file);
+      zip.generateAsync({ type: 'blob' }).then(content => {
+      const zipFile = new Blob([content]);
+      const url = URL.createObjectURL(zipFile);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'userFiles.zip';
+      link.click();
+});
+
+    
+
+
+
+    },
+    
   },
 };
 </script>
