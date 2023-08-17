351a70c24ba2445025788cbead8f2bcc5fcd71b3
doquan <doquangoncu@gmail.com>
2023-08-17 13:44:20 +0300

    .

--- secboard/src/main.js
+++ secboard/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import Sidebar from "@/components/Sidebar.vue"
 
+
 import router from './router'
 const app = createApp(App);
 app.component("Sidebar",Sidebar);
 app.use(router);
+app.use();
 app.mount("#app");
 
