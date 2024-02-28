eecc0b3c09e10447caba6ef093d1a4164f30d28c
doquan <doquangoncu@gmail.com>
2024-02-28 08:25:48 +0300

    28.02

--- 12-todo-app/src/components/TodoList.vue
+++ 12-todo-app/src/components/TodoList.vue
@@ -1,7 +1,5 @@
 <template>
   <ul>
-
-
     <li v-for="todo in todoList" :key="todo.id" class="d-flex justify-content-between align-items-center">
       <span> {{ todo.text }}</span>
       <button @click="deleteItem(todo)" class="sm red">Sil</button>
--- 6-lifcycle-hooks/app.js
+++ 6-lifcycle-hooks/app.js
@@ -12,9 +12,29 @@ const app = Vue.createApp({
 
     created(){
         console.log("created çalıştı");
+        /*
         setTimeout(() => {
          this.itemList = [1,2,3,4,5,6,7,8,9];
         }, 2000);
+        */
+        const maxProfit = (prices) => {
+            let left = 0; // Buy
+            let right = 1; // sell
+            let max_profit = 0;
+            while (right < prices.length) {
+              if (prices[left] < prices[right]) {
+                let profit = prices[right] - prices[left]; // our current profit
+          
+                max_profit = Math.max(max_profit, profit);
+              } else {
+                left = right;
+              }
+              right++;
+            }
+            return max_profit;
+          };
+          
+          console.log(`The solution is ${maxProfit([4,1,2,3])}`);
     },
     beforeMount(){
         console.log("beforeMount çalıştı");
